f100c3555048f23ac865739274072c4e237f4ce2
yaojie <limyaojie93@gmail.com>
2021-02-01 12:36:27 +0800

    Fix rubocop

--- spec/lib/myinfo/v3/response_spec.rb
+++ spec/lib/myinfo/v3/response_spec.rb
@@ -4,5 +4,5 @@ describe MyInfo::V3::Response do
   subject { described_class.new(success: true, data: 'some data') }
 
   it { expect(subject).to be_success }
-  it { expect("#{subject}").to eql('some data') }
+  it { expect(subject.to_s).to eql('some data') }
 end
