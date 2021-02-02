f3c53f60cd584e5b4291aaa0523db1c11c98db8e
yaojie <limyaojie93@gmail.com>
2021-02-02 16:22:28 +0800

    Update response specs

--- spec/lib/myinfo/v3/response_spec.rb
+++ spec/lib/myinfo/v3/response_spec.rb
@@ -1,8 +1,19 @@
 # frozen_string_literal: true
 
 describe MyInfo::V3::Response do
-  subject { described_class.new(success: true, data: 'some data') }
+  context 'successful' do
+    subject { described_class.new(success: true, data: 'some data') }
 
-  it { expect(subject).to be_success }
-  it { expect(subject.to_s).to eql('some data') }
+    it { expect(subject).to be_success }
+    it { expect(subject).not_to be_exception }
+    it { expect(subject.to_s).to eql('some data') }
+  end
+
+  context 'with exception' do
+    subject { described_class.new(success: false, data: Net::ReadTimeout.new('hello')) }
+
+    it { expect(subject).not_to be_success }
+    it { expect(subject).to be_exception }
+    it { expect(subject.to_s).to eql('Net::ReadTimeout with "hello"') }
+  end
 end
